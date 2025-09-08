b1d206f49728551b607cee17d5c48feda169b9a1
TrungTin29 <tin_dth235786@student.agu.edu.vn>
2025-09-08 09:32:29 +0700

    Chuong2

---
+++ Chuong1_LT.py
@@ -0,0 +1,57 @@
+print("Hello world!")
+
+print("Albert Einstein da tung noi: mot nguoi khong bao gio mac sai lam " \
+"khong bao gio thu bat ky dieu gi moi")
+
+if(5>2):
+    print(" Five is greater than two!")
+
+total = 1 +\
+2+\
+3
+print("total") ; print("Hello Python!")
+
+if(True):
+    print("True")
+else:
+    print("False")
+
+#This is a comment 
+print("Hello, World!")
+
+import math 
+import os
+def add(a,b):
+   result = a+b
+   return result
+
+import math_libs
+math_libs.add(4,5)
+
+#module1.py
+question = 'What is the meaning of life, the Universe, and Everthing?'
+answer = 42
+
+#module2.py
+question = 'What is your quest?'
+answer = "To seek the holy grail."
+
+import module1
+import module2
+print(module1.question)
+print(module2.question)
+print(module1.answer)
+print(module2.answer)
+
+#bai4tr34
+print("chao cac ban!")
+
+#Bai5tr34
+print("Phan Trung Tin")
+
+#Bai6tr34
+print("Minh ve minh co nho ta? ")
+print("Muoi lam nam ay thiet tha man nong")
+print("Minh ve minh co nho khong? ")
+print("Nhin cay nho nui, nhin song nho nguon")
+
